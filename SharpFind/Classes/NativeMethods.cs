7dd2cbc33038455c20b437355f9a11675ce3453c
Jad Altahan <isakovin@yahoo.com>
2017-01-13 10:06:16 -0800

    Change value spaceing for consistency

--- SharpFind/Classes/NativeMethods.cs
+++ SharpFind/Classes/NativeMethods.cs
@@ -636,29 +636,29 @@ namespace SharpFind.Classes
         internal static class WindowStylesFlags
         {
             internal static readonly uint
-            WS_BORDER = 0x00800000,
-            WS_CAPTION = 0x00C00000,
-            WS_CHILD = 0x40000000,
+            WS_BORDER       = 0x00800000,
+            WS_CAPTION      = 0x00C00000,
+            WS_CHILD        = 0x40000000,
             WS_CLIPCHILDREN = 0x02000000,
             WS_CLIPSIBLINGS = 0x04000000,
-            WS_DISABLED = 0x08000000,
-            WS_DLGFRAME = 0x00400000,
-            WS_GROUP = 0x00020000,
-            WS_HSCROLL = 0x00100000,
-            WS_ICONIC = 0x20000000,
-            WS_MAXIMIZE = 0x01000000,
-            WS_MAXIMIZEBOX = 0x00010000,
-            WS_MINIMIZE = 0x20000000,
-            WS_MINIMIZEBOX = 0x00020000,
-            WS_OVERLAPPED = 0x00000000,
-            WS_POPUP = 0x80000000,
-            WS_SIZEBOX = 0x00040000,
-            WS_SYSMENU = 0x00080000,
-            WS_TABSTOP = 0x00010000,
-            WS_THICKFRAME = 0x00040000,
-            WS_TILED = 0x00000000,
-            WS_VISIBLE = 0x10000000,
-            WS_VSCROLL = 0x00200000;
+            WS_DISABLED     = 0x08000000,
+            WS_DLGFRAME     = 0x00400000,
+            WS_GROUP        = 0x00020000,
+            WS_HSCROLL      = 0x00100000,
+            WS_ICONIC       = 0x20000000,
+            WS_MAXIMIZE     = 0x01000000,
+            WS_MAXIMIZEBOX  = 0x00010000,
+            WS_MINIMIZE     = 0x20000000,
+            WS_MINIMIZEBOX  = 0x00020000,
+            WS_OVERLAPPED   = 0x00000000,
+            WS_POPUP        = 0x80000000,
+            WS_SIZEBOX      = 0x00040000,
+            WS_SYSMENU      = 0x00080000,
+            WS_TABSTOP      = 0x00010000,
+            WS_THICKFRAME   = 0x00040000,
+            WS_TILED        = 0x00000000,
+            WS_VISIBLE      = 0x10000000,
+            WS_VSCROLL      = 0x00200000;
         }
 
         /// <summary>
@@ -668,30 +668,30 @@ namespace SharpFind.Classes
         internal static class ExtendedStylesFlags
         {
             internal static readonly uint
-            WS_EX_ACCEPTFILES = 0x00000010,
-            WS_EX_APPWINDOW = 0x00040000,
-            WS_EX_CLIENTEDGE = 0x00000200,
-            WS_EX_COMPOSITED = 0x02000000,
-            WS_EX_CONTEXTHELP = 0x00000400,
-            WS_EX_CONTROLPARENT = 0x00010000,
-            WS_EX_DLGMODALFRAME = 0x00000001,
-            WS_EX_LAYERED = 0x00080000,
-            WS_EX_LAYOUTRTL = 0x00400000,
-            WS_EX_LEFT = 0x00000000,
-            WS_EX_LEFTSCROLLBAR = 0x00004000,
-            WS_EX_LTRREADING = 0x00000000,
-            WS_EX_MDICHILD = 0x00000040,
-            WS_EX_NOACTIVATE = 0x08000000,
+            WS_EX_ACCEPTFILES     = 0x00000010,
+            WS_EX_APPWINDOW       = 0x00040000,
+            WS_EX_CLIENTEDGE      = 0x00000200,
+            WS_EX_COMPOSITED      = 0x02000000,
+            WS_EX_CONTEXTHELP     = 0x00000400,
+            WS_EX_CONTROLPARENT   = 0x00010000,
+            WS_EX_DLGMODALFRAME   = 0x00000001,
+            WS_EX_LAYERED         = 0x00080000,
+            WS_EX_LAYOUTRTL       = 0x00400000,
+            WS_EX_LEFT            = 0x00000000,
+            WS_EX_LEFTSCROLLBAR   = 0x00004000,
+            WS_EX_LTRREADING      = 0x00000000,
+            WS_EX_MDICHILD        = 0x00000040,
+            WS_EX_NOACTIVATE      = 0x08000000,
             WS_EX_NOINHERITLAYOUT = 0x00100000,
-            WS_EX_NOPARENTNOTIFY = 0x00000004,
-            WS_EX_RIGHT = 0x00001000,
-            WS_EX_RIGHTSCROLLBAR = 0x00000000,
-            WS_EX_RTLREADING = 0x00002000,
-            WS_EX_STATICEDGE = 0x00020000,
-            WS_EX_TOOLWINDOW = 0x00000080,
-            WS_EX_TOPMOST = 0x00000008,
-            WS_EX_TRANSPARENT = 0x00000020,
-            WS_EX_WINDOWEDGE = 0x00000100;
+            WS_EX_NOPARENTNOTIFY  = 0x00000004,
+            WS_EX_RIGHT           = 0x00001000,
+            WS_EX_RIGHTSCROLLBAR  = 0x00000000,
+            WS_EX_RTLREADING      = 0x00002000,
+            WS_EX_STATICEDGE      = 0x00020000,
+            WS_EX_TOOLWINDOW      = 0x00000080,
+            WS_EX_TOPMOST         = 0x00000008,
+            WS_EX_TRANSPARENT     = 0x00000020,
+            WS_EX_WINDOWEDGE      = 0x00000100;
         }
 
         /// <summary>
@@ -701,14 +701,14 @@ namespace SharpFind.Classes
         internal static class PriorityClass
         {
             internal static readonly uint
-            NORMAL_PRIORITY_CLASS = 0x20,
-            IDLE_PRIORITY_CLASS = 0x40,
-            HIGH_PRIORITY_CLASS = 0x80,
-            REALTIME_PRIORITY_CLASS = 0x100,
-            BELOW_NORMAL_PRIORITY_CLASS = 0x4000,
-            ABOVE_NORMAL_PRIORITY_CLASS = 0x8000,
+            NORMAL_PRIORITY_CLASS         = 0x20,
+            IDLE_PRIORITY_CLASS           = 0x40,
+            HIGH_PRIORITY_CLASS           = 0x80,
+            REALTIME_PRIORITY_CLASS       = 0x100,
+            BELOW_NORMAL_PRIORITY_CLASS   = 0x4000,
+            ABOVE_NORMAL_PRIORITY_CLASS   = 0x8000,
             PROCESS_MODE_BACKGROUND_BEGIN = 0x100000,
-            PROCESS_MODE_BACKGROUND_END = 0x200000;
+            PROCESS_MODE_BACKGROUND_END   = 0x200000;
         }
 
         /// <summary>
@@ -720,17 +720,17 @@ namespace SharpFind.Classes
             internal static readonly int
             CS_BYTEALIGNCLIENT = 0x1000,
             CS_BYTEALIGNWINDOW = 0x2000,
-            CS_CLASSDC = 0x0040,
-            CS_DBLCLKS = 0x0008,
-            CS_DROPSHADOW = 0x00020000,
-            CS_GLOBALCLASS = 0x4000,
-            CS_HREDRAW = 0x0002,
-            CS_IME = 0x00010000,
-            CS_NOCLOSE = 0x0200,
-            CS_OWNDC = 0x0020,
-            CS_PARENTDC = 0x0080,
-            CS_SAVEBITS = 0x0800,
-            CS_VREDRAW = 0x0001;
+            CS_CLASSDC         = 0x0040,
+            CS_DBLCLKS         = 0x0008,
+            CS_DROPSHADOW      = 0x00020000,
+            CS_GLOBALCLASS     = 0x4000,
+            CS_HREDRAW         = 0x0002,
+            CS_IME             = 0x00010000,
+            CS_NOCLOSE         = 0x0200,
+            CS_OWNDC           = 0x0020,
+            CS_PARENTDC        = 0x0080,
+            CS_SAVEBITS        = 0x0800,
+            CS_VREDRAW         = 0x0001;
         }
 
         #endregion
