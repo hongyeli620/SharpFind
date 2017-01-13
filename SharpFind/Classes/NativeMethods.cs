72fb30dfe60657203e473bc6f2b8867ed8a8775d
Jad Altahan <isakovin@yahoo.com>
2017-01-13 10:07:40 -0800

    Change value spacing for consistency

--- SharpFind/Classes/NativeMethods.cs
+++ SharpFind/Classes/NativeMethods.cs
@@ -121,29 +121,29 @@ namespace SharpFind.Classes
         // used by the program
         internal enum WindowsMessages : uint
         {
-            NULL = 0x00,
+            NULL      = 0x00,
             MOUSEMOVE = 0x200,
             LBUTTONUP = 0x202
         }
 
         internal enum RasterOperations : uint
         {
-            BLACKNESS = 0x00000042,
-            CAPTUREBLT = 0x40000000,
-            DSTINVERT = 0x00550009,
-            MERGECOPY = 0x00C000CA,
-            MERGEPAINT = 0x00BB0226,
-            NOTSRCCOPY = 0x00330008,
+            BLACKNESS   = 0x00000042,
+            CAPTUREBLT  = 0x40000000,
+            DSTINVERT   = 0x00550009,
+            MERGECOPY   = 0x00C000CA,
+            MERGEPAINT  = 0x00BB0226,
+            NOTSRCCOPY  = 0x00330008,
             NOTSRCERASE = 0x001100A6,
-            PATCOPY = 0x00F00021,
-            PATINVERT = 0x005A0049,
-            PATPAINT = 0x00FB0A09,
-            SRCAND = 0x008800C6,
-            SRCCOPY = 0x00CC0020,
-            SRCERASE = 0x00440328,
-            SRCINVERT = 0x00660046,
-            SRCPAINT = 0x00EE0086,
-            WHITENESS = 0x00FF0062
+            PATCOPY     = 0x00F00021,
+            PATINVERT   = 0x005A0049,
+            PATPAINT    = 0x00FB0A09,
+            SRCAND      = 0x008800C6,
+            SRCCOPY     = 0x00CC0020,
+            SRCERASE    = 0x00440328,
+            SRCINVERT   = 0x00660046,
+            SRCPAINT    = 0x00EE0086,
+            WHITENESS   = 0x00FF0062
         }
 
         #endregion
