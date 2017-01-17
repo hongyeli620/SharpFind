3abf90a136fcc80f2d98b5afee1fd5f85b9a44d2
Jad Altahan <isakovin@yahoo.com>
2017-01-16 17:44:53 -0800

    Add TreeViewControlStyles definitions

--- SharpFind/Classes/NativeMethods.cs
+++ SharpFind/Classes/NativeMethods.cs
@@ -1114,6 +1114,30 @@ namespace SharpFind.Classes
             TBS_TRANSPARENTBKGND = 0x1000;
         }
 
+        /// <summary>
+        /// Based on Windows Kits\8.1\CommCtrl.h
+        /// </summary>
+        internal static class TreeViewControlStyles
+        {
+            internal static readonly int
+            TVS_HASBUTTONS      = 0x0001,
+            TVS_HASLINES        = 0x0002,
+            TVS_LINESATROOT     = 0x0004,
+            TVS_EDITLABELS      = 0x0008,
+            TVS_DISABLEDRAGDROP = 0x0010,
+            TVS_SHOWSELALWAYS   = 0x0020,
+            TVS_RTLREADING      = 0x0040,
+            TVS_NOTOOLTIPS      = 0x0080,
+            TVS_CHECKBOXES      = 0x0100,
+            TVS_TRACKSELECT     = 0x0200,
+            TVS_SINGLEEXPAND    = 0x0400,
+            TVS_INFOTIP         = 0x0800,
+            TVS_FULLROWSELECT   = 0x1000,
+            TVS_NOSCROLL        = 0x2000,
+            TVS_NONEVENHEIGHT   = 0x4000,
+            TVS_NOHSCROLL       = 0x8000;
+        }
+
         #endregion
         #region Process Priority Definitions
 
