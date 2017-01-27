d39b3f03aa192d93e3a7a50cd0a16b6dd9f04718
Jad Altahan <isakovin@yahoo.com>
2017-01-27 00:12:41 -0800

    Add copyright notice

--- SharpFind/Classes/INIReadWrite.cs
+++ SharpFind/Classes/INIReadWrite.cs
@@ -1,4 +1,12 @@
-﻿using System.Windows.Forms;
+﻿/* INIReadWrite.cs
+** This file is part #Find.
+** 
+** Copyright 2017 by Jad Altahan <hello@exr.be>
+** Licensed under MIT
+** <https://github.com/ei/SharpFind/blob/master/LICENSE>
+*/
+
+using System.Windows.Forms;
 using System;
 
 using static SharpFind.Classes.NativeMethods;
