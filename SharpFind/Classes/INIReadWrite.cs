4f5a11f31531f8adf47f2721c1d71de42d5ba79d
Jad Altahan <isakovin@yahoo.com>
2017-01-26 23:00:05 -0800

    Update INIReadWrite.cs

--- SharpFind/Classes/INIReadWrite.cs
+++ SharpFind/Classes/INIReadWrite.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows.Forms;
 using System;
+
 using static SharpFind.Classes.NativeMethods;
 
 namespace SharpFind.Classes
@@ -23,14 +24,14 @@ namespace SharpFind.Classes
 
         public static string INIRead(string path, string section, string key, string defaultValue)
         {
-            var returnValue = string.Empty;
+            var value = string.Empty;
             var sData = string.Empty;
 
             sData = new string(' ', 1024);
             var i = Convert.ToInt32(GetPrivateProfileString(section, key, defaultValue, sData, sData.Length, path));
-            returnValue = i > 0 ? sData.Substring(0, i) : string.Empty;
+            value = i > 0 ? sData.Substring(0, i) : string.Empty;
 
-            return returnValue;
+            return value;
         }
 
         public static void INIWrite(string path, string section, string key, string value)
