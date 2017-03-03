aeed0332ad36b9f168464d256819b743167474ac
Jad Altahan <isakovin@yahoo.com>
2017-03-02 22:58:36 -0800

    Add header comment

--- SharpFind/Controls/LinkLabelEx.cs
+++ SharpFind/Controls/LinkLabelEx.cs
@@ -1,4 +1,12 @@
-﻿using System.Drawing;
+﻿/* LinkLabelEx.cs
+** This file is part #Find.
+** 
+** Copyright 2017 by Jad Altahan <hello@exr.be>
+** Licensed under MIT
+** <https://github.com/ei/SharpFind/blob/master/LICENSE>
+*/
+
+using System.Drawing;
 using System.Windows.Forms;
 using System;
 using SharpFind.Classes;
